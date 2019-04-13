8ccb2a385fe1e60da8a49c5e685978d01ea642fb
yeetyeet <pojaluci@easy-apps.info>
2019-04-13 08:08:46 -0700

    ?

---
+++ src/extra/adventure.js
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello</title>
+	<linl rel="stylesheet" href="style.css" />
+	<script scr="towerwalls8.jpg"></script>
+</head>
+<body>
+<div class+"header">
+	<h1>Hello</h1>
+	<p>Hello</p>
+</div> 
+
+<div class="container">
+
+</div>
+<script>
+
+</scripts>
+</body>
+</html>
